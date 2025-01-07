51efe08ad0138c103ca74e983cc6921ce242ded1
bclinks12 <96894259+bclinks12@users.noreply.github.com>
2025-01-07 08:15:08 -0800

    Add files via upload

---
+++ prompts.ps1
@@ -0,0 +1,23 @@
+# Brittany Clinkscales - Student ID: 001265256
+<#
+    .SYNOPSIS
+    This script will prompt end user input for various task.
+    
+    .DESCRIPTION
+    This script provides a menu end user interface for perfroming task present in the Requirements folder.
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    The selections are displayed via the end users screen.
+
+    .EXAMPLE
+    Run this by typing PS> .\prompts.ps1
+
+    .NOTES
+    Author: Brittany Clinkscales
+    Student ID: 001265256
+    Start Date: 01/07/2025
+
+#>
