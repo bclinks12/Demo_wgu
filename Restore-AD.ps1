014bc0794d73fa29291873045876efba054b09df
bclinks12 <96894259+bclinks12@users.noreply.github.com>
2025-01-09 11:55:14 -0800

    Update Restore-AD.ps1
    this is the version you submit to wgu

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,9 +1,37 @@
 # Brittany Clinkscales - Student ID: 001265256
-#New-ADOrganizationalUnit -Name $OUCanonicalName -Path $AdRoot -Description "Finance"
 
+#1.
+try {
+    #Check if the OU - Finance exists
+    $OU = Get-ADOrganizationalUnit -Identity $ADPath
+    Write-Host "OU - Finance already exists!" -ForegroundColor Green
+}
+catch {
+    #If the OU - Finance doesn't exist
+    $OU = $null
+    Write-Host "OU - Finance not found!" -ForegroundColor Red
+}
+
+if ($OU) {
+    Set-ADOrganizationalUnit -Identity $ADPath -ProtectedFromAccidentalDeletion:$false
+    Remove-ADOrganizationalUnit -Identity $ADPath -Recursive -Confirm:$false
+    Write-Host "OU - Finance has been deleted" -ForegroundColor Red
+} else {
+    Write-Host "OU - Finance no longer exits!" -ForegroundColor Green
+}
+
+#2.
+#code to add new OU - Finance
+New-ADOrganizationalUnit -Name $OUCanonicalName -Path $AdRoot -Description "Finance"
+#Output message
+Write-Host "OU - Finance creation is complete."
+
+
+#3.
+#Code to add new users to OU - Finanace
 #Define Variables 
 $OUPath = "OU=Finance,DC=consultingfirm,DC=com"
-$DnsRoot = "consultingfirm.com"
+$DnsRoot = "consutlingfirm.com"
 
 $NewADUsers = Import-Csv $PSScriptRoot\financePersonnel.csv
 
@@ -18,8 +46,11 @@ ForEach ($ADUser in $NewADUsers) {
         PostalCode          = $($ADUser.PostalCode)
         MobilePhone         = $($ADUser.MobilePhone)
         OfficePhone         = $($ADUser.OfficePhone )
-        Path                = $ADPath
+        Path                = $OUPath
     }
     New-ADUser @Attributes
 }
-#Get-ADUser -Filter * -SearchBase "ou=Finance,dc=consultingfirm,dc=com" -Properties DisplayName,PostalCode,OfficePhone,MobilePhone >.AdResults.-AsPlainText
+
+
+#Code generates output file for submission
+Get-ADUser -Filter * -SearchBase "ou=Finance,dc=consultingfirm,dc=com" -Properties DisplayName,PostalCode,OfficePhone,MobilePhone >.AdResults.-AsPlainText
